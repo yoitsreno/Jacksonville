163baa465a9418832c1ba5d55889275180628ed7
yoitsreno <41997819+yoitsreno@users.noreply.github.com>
2019-02-13 01:19:58 +0700

    Add files via upload

--- starter/script.js
+++ starter/script.js
@@ -157,3 +157,50 @@ if (age < 20) {
 * The Ternary Operator and Switch Statements
 */
 
+var firstName = 'John';
+var age = '14';
+
+// Ternary operator
+age >= 18 ? console.log(firstName + ' drinks beer.')
+: console.log(firstName + ' drinks juice.');
+var drink = 18 >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+/*if (age >=18) {
+    var drink = 'beer';
+} else { 
+    var drink = 'juice';
+}*/
+
+// Swith statement
+    var job = 'instructor';
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites');
+        break;
+    default:
+        console.log(firstName + ' does something else');
+
+}
+
+age = 56; 
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy. ');
+    break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager. ');
+    break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man. ');
+    break;
+    default:
+    console.log(firstName + ' is a man. ');
+}
